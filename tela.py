735ca24800ffdb53307a3e718715a628e86acfed
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-21 01:41:10 -0300

    Finalizado V1.0

--- tela.py
+++ tela.py
@@ -3,6 +3,8 @@ import PySimpleGUI as sg
 # Criação commpleta da tela
 class TelaPython:
     def __init__(self):
+
+        sg.change_look_and_feel('Reddit')
         # Layout
         layout = [
             [sg.Text('Nome', size=(5,0)),sg.Input(size=(15,0),key='Nome')],
@@ -11,31 +13,38 @@ class TelaPython:
             [sg.Checkbox('Gmail',key='Gmail'),sg.Checkbox('Outlook',key='Outlook'),sg.Checkbox('Yahoo',key='Yahoo')],
             [sg.Text('Aceita Cartão?')],
             [sg.Radio('Sim','cartoes',key='AceitaCartao'),sg.Radio('Não', 'cartoes', key='NaoAceita')],
-            [sg.Button('Enviar Dados')]
+            [sg.Slider(range=(0,100),default_value=0, orientation='h', size=(15,20), key='SliderVelocidade')],
+            [sg.Button('Enviar Dados')],
+            [sg.Output(size=(30,20))]
         ]
 
         # Janela
-        janela = sg.Window("Dados do usuário").layout(layout)
+        self.janela = sg.Window("Dados do usuário").layout(layout)
 
-        # Extrair os dados da tela
-        self.button, self.values = janela.Read()
+
+        
 
     def Iniciar(self):
-        nome = self.values['Nome']
-        idade = self.values['Idade']
-        aceita_gmail = self.values['Gmail']
-        aceita_outlook = self.values['Outlook']
-        aceita_yahoo = self.values['Yahoo']
-        cartao_sim = self.values['AceitaCartao']
-        cartao_nao = self.values['NaoAceita']
+        while True:
+            # Extrair os dados da tela continuamente
+            self.button, self.values = self.janela.Read()
+            nome = self.values['Nome']
+            idade = self.values['Idade']
+            aceita_gmail = self.values['Gmail']
+            aceita_outlook = self.values['Outlook']
+            aceita_yahoo = self.values['Yahoo']
+            cartao_sim = self.values['AceitaCartao']
+            cartao_nao = self.values['NaoAceita']
+            velocidade_script = self.values['SliderVelocidade']
 
-        print(f'Nome: {nome}')
-        print(f'Idade: {idade}')
-        print(f'Aceita Gmail: {aceita_gmail}')
-        print(f'Aceita Outlook: {aceita_outlook}')
-        print(f'Aceita Yahoo: {aceita_yahoo}')
-        print(f'Aceita Cartão: {cartao_sim}')
-        print(f'Não Aceita: {cartao_nao}')
+            print(f'Nome: {nome}')
+            print(f'Idade: {idade}')
+            print(f'Aceita Gmail: {aceita_gmail}')
+            print(f'Aceita Outlook: {aceita_outlook}')
+            print(f'Aceita Yahoo: {aceita_yahoo}')
+            print(f'Aceita Cartão: {cartao_sim}')
+            print(f'Não Aceita: {cartao_nao}')
+            print(f'Velocidade Scripts: {velocidade_script}')
 
 tela = TelaPython()
 tela.Iniciar()
