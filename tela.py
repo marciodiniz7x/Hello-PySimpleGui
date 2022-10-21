eaa453ff16e5eea0361a363897cf80e66d4a79a9
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-21 01:21:35 -0300

    Adicionei mais botões

--- tela.py
+++ tela.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
 
+# Criação commpleta da tela
 class TelaPython:
     def __init__(self):
         # Layout
         layout = [
-            [sg.Text('Nome'),sg.Input()],
-            [sg.Text('Idade'),sg.Input()],
+            [sg.Text('Nome', size=(5,0)),sg.Input(size=(15,0),key='Nome')],
+            [sg.Text('Idade', size=(5,0)),sg.Input(size=(15,0),key='Idade')],
+            [sg.Text('Quais provedores de email são aceitos?')],
+            [sg.Checkbox('Gmail',key='Gmail'),sg.Checkbox('Outlook',key='Outlook'),sg.Checkbox('Yahoo',key='Yahoo')],
+            [sg.Text('Aceita Cartão?')],
+            [sg.Radio('Sim','cartoes',key='AceitaCartao'),sg.Radio('Não', 'cartoes', key='NaoAceita')],
             [sg.Button('Enviar Dados')]
         ]
 
@@ -16,7 +21,21 @@ class TelaPython:
         self.button, self.values = janela.Read()
 
     def Iniciar(self):
-        print(self.values)
+        nome = self.values['Nome']
+        idade = self.values['Idade']
+        aceita_gmail = self.values['Gmail']
+        aceita_outlook = self.values['Outlook']
+        aceita_yahoo = self.values['Yahoo']
+        cartao_sim = self.values['AceitaCartao']
+        cartao_nao = self.values['NaoAceita']
+
+        print(f'Nome: {nome}')
+        print(f'Idade: {idade}')
+        print(f'Aceita Gmail: {aceita_gmail}')
+        print(f'Aceita Outlook: {aceita_outlook}')
+        print(f'Aceita Yahoo: {aceita_yahoo}')
+        print(f'Aceita Cartão: {cartao_sim}')
+        print(f'Não Aceita: {cartao_nao}')
 
 tela = TelaPython()
 tela.Iniciar()
